749f004de8d729c52f37d10708218aa280cac8de
Amit Kumar <kamit05518@gmail.com>
2025-10-04 11:51:53 +0530

    Update profile routes

--- Routes/api/profile.Routes.js
+++ Routes/api/profile.Routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
-const Profile = require('../../Controller/Profile.js');
+const Profile = require('../../Controller/profile');
 
 // Multer config
 const storage = multer.diskStorage({
